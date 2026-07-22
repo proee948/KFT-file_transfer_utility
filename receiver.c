d4dc25aca056bb72626d1f4a363fb132a98520cc
Toma Barbarić <tomabarbaric1@gmail.com>
2026-07-22 18:10:53 +0200

    Implement socket connection handling
    Added client acceptance and connection logging.

--- receiver.c
+++ receiver.c
@@ -2,9 +2,6 @@
 
 int main()
 {
-    //char *ptr_ip = extract_ip(); //extracted ip of RECEIVER
-
-    //setup Inbound Socket
     int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     
     int opt = 1;
@@ -19,5 +16,22 @@ int main()
     bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     listen(listen_fd, 10);
 
+    printf("Listening...\n");
 
-}   
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    
+    int client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len);
+    if (client_fd < 0) {
+        perror("Error");
+        close(listen_fd);
+        return 1;
+    }
+
+    printf("Connected: %s\n", inet_ntoa(client_addr.sin_addr));
+
+    close(client_fd);
+    close(listen_fd);
+
+    return 0;
+}
