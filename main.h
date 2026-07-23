4b2a5bfd5f5312dbf723b2247a7631a68295a3bc
Toma Barbarić <tomabarbaric1@gmail.com>
2026-07-23 15:03:41 +0200

    Add function prototype for send_file

--- main.h
+++ main.h
@@ -9,11 +9,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/fcntl.h>
+#include <sys/sendfile.h>
 
 #define MAX_IP_LENGTH 128
 
 struct info{char path[1024];};
 
+void send_file(struct info);
 struct info get_path(void);
 char* extract_ip(void);
 void start_tcp(void);
